8e1df2b22977d5476bb73186f14ede11a2b2e803
Whitney Griggs <wsgriggs@gmail.com>
2018-07-10 17:45:19 -0700

    Subject 101-1 files

---
+++ pay_subj_temp.m
@@ -0,0 +1,26 @@
+function pay_subj_temp(subID)
+
+
+
+%% pay_subj('999-1')
+%% pay_subj('004-3')
+
+% item id
+%under 100 for food, over 100 for trinkets
+%003
+%item_id = [9 37];
+
+%004
+item_id = [70 112];
+
+%flip a coin to determine whether trial is from BDM or choice trials
+p = rand;
+if p > 0.5
+    %BDM
+    pay_subj_BDM(subID,item_id);
+else
+    %CHOICE
+    pay_subj_choice(subID, item_id);
+end
+
+end
